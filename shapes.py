eb3bdb688960e53f2b622cd7aebd265079a10d84
leo parry <leonardo.parry@gmail.com>
2014-10-11 10:00:49 +0000

    next version

--- shapes.py
+++ shapes.py
@@ -1,5 +1,9 @@
-Python 3.2.3 (default, Mar  1 2013, 11:53:50) 
-[GCC 4.6.3] on linux2
-Type "copyright", "credits" or "license()" for more information.
->>> from turtle import*
->>> 
+from turtle import*
+forward(100)
+left (90)
+pencolor ('red')
+forward(100)
+pencolor ('blue')
+left (90)
+forward(100)
+
