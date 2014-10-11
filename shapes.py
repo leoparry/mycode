7a8a3b3ffc2cb1acc8405f1ad5bb67e14d51f7bf
leo parry <leonardo.parry@gmail.com>
2014-10-11 14:49:54 +0000

    next version

--- shapes.py
+++ shapes.py
@@ -1,5 +1,7 @@
 from turtle import*
 import time
+pensize (50)
+turtlesize (5)
 forward(100)
 left (90)
 pencolor ('red')
@@ -7,6 +9,18 @@ forward(100)
 pencolor ('blue')
 left (90)
 forward(100)
+pencolor ('yellow')
+left (90)
+forward(100)
+left (180)
+forward (50)
+right(90)
+pencolor('green')
+for x in range (100):
+    forward(1)
+    
+
+
 
 time.sleep(10)
 
